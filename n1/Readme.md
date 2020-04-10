adbd7924f9c31fa23c24af84f24e5ef52f1da6f7
molun <xinhiman@126.com>
2020-04-10 18:00:44 +0800

    update

---
+++ n1/Readme.md
@@ -0,0 +1,2 @@
+编译N1适用的适用docker运行的openwrt镜像  
+基于Lean和Lienol源码  
--- n1/diy.sh
+++ n1/diy.sh
@@ -1,1 +1,1 @@
-sed -i 's/192.168.1.1/192.168.2.3/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
