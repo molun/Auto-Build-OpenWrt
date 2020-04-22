8350d588bab6edc4e0ae6f639537d64217c3eae8
molun <xinhiman@126.com>
2020-04-22 17:40:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Define My Package
+git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
