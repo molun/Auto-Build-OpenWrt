46181bfdd2b3c728c5561dfe6ee23960df8a40b1
molun <xinhiman@126.com>
2020-05-04 19:55:36 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Define My Package
 #git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
-git clone https://github.com/vernesong/OpenClash.git                                package/molun/luci-app-openclash
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
+#git clone https://github.com/vernesong/OpenClash.git                                package/molun/luci-app-openclash
