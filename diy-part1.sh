d849efe98d7c9b9d66a09ab5e1ee28342c8ec8db
molun <xinhiman@126.com>
2020-05-04 15:03:39 +0800

    update

---
+++ diy-part1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Define My Package
+#git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
+git clone https://github.com/vernesong/OpenClash.git                                package/molun/luci-app-openclash
