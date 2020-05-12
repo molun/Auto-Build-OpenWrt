aa781faef0ed6d2cec7c469122a934f0426542c5
molun <xinhiman@126.com>
2020-05-12 19:42:37 +0800

    UPDATE

--- diy-part3.sh
+++ diy-part3.sh
@@ -9,12 +9,12 @@
 #=============================================================
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Define My Package
-#git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
 git clone https://github.com/vernesong/OpenClash.git                                package/molun/luci-app-openclash
