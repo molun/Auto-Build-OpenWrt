7ede4f3394ed26fdf6f57fd896f3aa609947212d
molun <xinhiman@126.com>
2020-10-31 14:09:17 +0800

    Update diy-part0.sh

--- diy-part0.sh
+++ diy-part0.sh
@@ -10,11 +10,10 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-#git clone https://github.com/vernesong/luci-app-clash package/molun/luci-app-clash
-#git clone https://github.com/fw876/helloworld package/molun/luci-app-ssr-plus
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#git clone https://github.com/frainzy1477/luci-app-clash package/frainzy1477/luci-app-clash
+#git clone https://github.com/fw876/helloworld package/fw876/luci-app-ssr-plus
