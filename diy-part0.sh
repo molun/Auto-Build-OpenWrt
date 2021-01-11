6e1b8ce4803aefd02ac99cca80c861a90558a2f0
molun <xinhiman@126.com>
2021-01-11 22:44:25 +0800

    update
    update

--- diy-part0.sh
+++ diy-part0.sh
@@ -15,5 +15,3 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#git clone https://github.com/frainzy1477/luci-app-clash package/frainzy1477/luci-app-clash
-#git clone https://github.com/fw876/helloworld package/fw876/luci-app-ssr-plus
--- diy-part1.sh
+++ diy-part1.sh
@@ -11,16 +11,16 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Define My Package
 #git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
-git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/molun/luci-theme-infinityfreedom
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
+#git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
+#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/molun/luci-theme-infinityfreedom
