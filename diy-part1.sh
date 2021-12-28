cf6bc326fe6bdb69f2b6c2881891806ce19e0bc7
molun <xinhiman@126.com>
2021-12-28 13:41:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,7 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 # Define My Package
 #git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
