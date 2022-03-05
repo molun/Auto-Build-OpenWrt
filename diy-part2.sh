0f8eb4571f50b35870f7ec0800abeb4eceb507c0
molun <xinhiman@126.com>
2022-03-05 18:31:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,5 +8,15 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
+# 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 删除默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+# 删除默认主题
+rm -rf package/lean/luci-theme-argon
+
+# 取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
