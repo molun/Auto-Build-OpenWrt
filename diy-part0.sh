db07dadf5c90cd009dcaeffcfb07dfd8d79b360c
molun <xinhiman@126.com>
2022-03-06 13:22:57 +0800

    Update diy-part0.sh

--- diy-part0.sh
+++ diy-part0.sh
@@ -16,17 +16,17 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom' feeds.conf.default
  
 # Define My Package
 git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
-git clone https://github.com/zzsj0928/luci-app-pushbot package/molun/luci-app-pushbot
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
 #git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
+git clone https://github.com/zzsj0928/luci-app-pushbot package/molun/luci-app-pushbot
 git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/molun/luci-theme-edge
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/molun/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/molun/luci-app-argon-config
